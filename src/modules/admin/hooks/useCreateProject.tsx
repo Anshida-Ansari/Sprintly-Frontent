8330479505eb5a6d4d6744952fb8638651fb3106
Anshida-Ansari <anshidaansari9a@gmail.com>
2026-01-21 12:33:31 +0530

    feat(project-creation):reload state issue is resolved

--- src/modules/admin/hooks/useCreateProject.tsx
+++ src/modules/admin/hooks/useCreateProject.tsx
@@ -1,13 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { projectService } from "../services/project.service";
 import type { CreateProjectPayload } from "../types/types";
 
 export function useCreateProject() {
+    const queryClient = useQueryClient();
+
     return useMutation({
         mutationFn: (payload: CreateProjectPayload) => projectService.createProject(payload),
         onSuccess: (res) => {
             toast.success(res.message || "Project created successfully");
+            queryClient.invalidateQueries({ queryKey: ['projects'] });
         },
         onError: (err: any) => {
             toast.error(err.response?.data?.message || "Failed to create project");
